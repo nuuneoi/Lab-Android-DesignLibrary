826ad31a6065a97b160a1a58b2af0298061f956d
nuuneoi <sittiphol@gmail.com>
2015-06-21 00:09:27 +0700

    Fix Hamburger icon clickable

--- app/src/main/java/com/inthecheesefactory/lab/designlibrary/MainActivity.java
+++ app/src/main/java/com/inthecheesefactory/lab/designlibrary/MainActivity.java
@@ -89,6 +89,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+        
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
